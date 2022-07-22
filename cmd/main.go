6382d5343cc72ba64b88751220e7391c98f0eae5
e92_git <web.kss.ru@gmail.com>
2022-07-22 18:21:12 +0700

    deploy

--- cmd/main.go
+++ cmd/main.go
@@ -42,7 +42,7 @@ var albums = []album{
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8080
+// @host      scan.e92.ru
 // @BasePath  /api/v1
 
 // @securityDefinitions.basic  BasicAuth
