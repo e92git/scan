f29492d3654d3592723a63a8cd5553f76791e511
e92_git <web.kss.ru@gmail.com>
2022-07-22 18:56:40 +0700

    swagger

--- docs/docs.go
+++ docs/docs.go
@@ -35,7 +35,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "scan.e92.ru",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Swagger Example API",
