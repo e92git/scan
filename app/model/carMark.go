9c68e97aada629dc4ca5e0a4b6662b2d7c913cf1
e92_git <web.kss.ru@gmail.com>
2022-09-13 23:19:38 +0700

    import script

--- app/model/carMark.go
+++ app/model/carMark.go
@@ -3,7 +3,8 @@ package model
 import "time"
 
 type CarMark struct {
-	ID        int       `json:"id" example:"12"`
-	Name      string    `json:"name" example:"Toyota" validate:"required"`
-	CreatedAt time.Time `json:"-" example:"2022-07-29T11:23:55.999+07:00"`
+	ID          int       `json:"id" example:"12"`
+	Name        string    `json:"name" example:"Toyota" validate:"required"`
+	NameInTires *string   `json:"name_in_tires" example:"Toyota"`
+	CreatedAt   time.Time `json:"-" example:"2022-07-29T11:23:55.999+07:00"`
 }
